4ca214b6ab5c48756a56bb2c859bde7c9f31d524
kasi <kasi1@gmail.com>
2024-08-15 11:36:27 +0530

    more commits

---
+++ 01-print.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# we are going to learn how to print statement with echo command.
+
+learner=kasi
+syallbus=$1
+date=$(date)
+
+
+echo my name is harish.may i know where are you and from.
+
+echo " ********* I am from The England and My name is Rose *********** "
+
+echo -e "\e[32m Welcome to Mother India Miss Rose.\e[0m"
+
+echo "My name is ${learner}."
+
+echo "we are learning $syallbus here."
+
+echo "Starting date of the class are : $date "
+
+
+
+
