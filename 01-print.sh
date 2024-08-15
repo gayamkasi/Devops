db9596433aa4c356187b113fbb9b96d14d1df95a
kasi <kasi1@gmail.com>
2024-08-15 10:25:52 +0530

    more commits

--- 01-print.sh
+++ 01-print.sh
@@ -3,4 +3,4 @@
 # we are going to learn how to print statement with echo command.
 
 echo hello this is rose.what is your name please.
-my name is harish.may i know where are you from.
+echo my name is harish.may i know where are you from.
