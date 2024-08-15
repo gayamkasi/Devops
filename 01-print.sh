aa358815a80716fbb02576ff1ea3011b77a6149f
kasi <kasi1@gmail.com>
2024-08-15 10:58:35 +0530

    more commits

--- 01-print.sh
+++ 01-print.sh
@@ -4,6 +4,8 @@
 
 learner=kasi
 professor=rambabu
+syallbus=$1
+
 
 echo hello this is rose.what is your name please.
 echo my name is harish.may i know where are you from.
@@ -13,7 +15,8 @@ echo " ********* I am from The England *********** "
 echo -e "\e[32m Welcome to Mother India Miss Rose.\e[0m"
 
 echo "what is the name of student roll no 1 ${learner}."
-echo "what is your $professor name." 
+echo "what is your Trainer $professor name." 
+echo "what are you going to learn here $syallbus ."
 
 
 
