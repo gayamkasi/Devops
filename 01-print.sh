6769a3f1f03fe304298ca68f9000557068394f2b
kasi <kasi1@gmail.com>
2024-08-15 11:18:06 +0530

    more commits

--- 01-print.sh
+++ 01-print.sh
@@ -6,6 +6,7 @@ learner=kasi
 professor=rambabu
 syallbus=$1
 date=$(date)
+dollar=$2
 
 
 echo hello this is rose.what is your name please.
@@ -17,11 +18,13 @@ echo -e "\e[32m Welcome to Mother India Miss Rose.\e[0m"
 
 echo "My name is ${learner}."
 
-echo "Our Trainer $professor name." 
+echo "Our Trainer $professor sir." 
 
 echo "we are learning $syallbus here."
 
 echo "Starting date of the class are : $date "
+
+echo "what is the value of $ today in india - $2"
 
 
 
--- shell.sh
+++ shell.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # we are going to learn  shell and improve scripting language techniques
-Hi welcome to shell programming
 
 01 Echo statements and Comments
 02 Quotes
-03 Color code syntax
-04 Variable
+03 Color Code Syntax
+04 Variable and its Three Properities
