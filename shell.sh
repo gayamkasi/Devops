d7ef2cde094266c987a5f077578ed94e921152f8
kasi <kasi1@gmail.com>
2024-08-15 10:52:06 +0530

    more commits

--- shell.sh
+++ shell.sh
@@ -2,7 +2,7 @@
 # we are going to learn  shell and improve scripting language techniques
 Hi welcome to shell programming
 
-01 Echo statements
+01 Echo statements and Comments
 02 Quotes
 03 Color code syntax
 04 Variable
